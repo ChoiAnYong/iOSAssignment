f3ea029578bd3873e623ada005fa5222baefd490
ChoiAnYong <dksdyd78@naver.com>
2025-08-19 22:47:17 +0900

    setting: 프로젝트 세팅

---
+++ iOSAssignment/iOSAssignment/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAssignment
+//
+//  Created by 최안용 on 8/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
