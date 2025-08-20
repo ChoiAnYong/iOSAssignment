ab4d0fb2d3a8fded9b58b7474195530f1648a090
ChoiAnYong <dksdyd78@naver.com>
2025-08-21 00:47:31 +0900

    feat: StartViewController 구현

---
+++ iOSAssignment/iOSAssignment/Presentation/StartView/ViewController/StartViewController.swift
@@ -0,0 +1,63 @@
+//
+//  StartViewController.swift
+//  iOSAssignment
+//
+//  Created by 최안용 on 8/19/25.
+//
+
+import UIKit
+
+import RxSwift
+
+final class StartViewController: UIViewController {
+    private let rootView = StartView()
+    private let viewModel: StartViewModel
+    private let disposeBag = DisposeBag()
+    
+    init(viewModel: StartViewModel) {
+        self.viewModel = viewModel
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bindViewModel()
+    }
+}
+
+extension StartViewController {
+    private func bindViewModel() {
+        let input = StartViewModel.Input(
+            startTrigger: rootView.startButton.rx.tap.asObservable()
+        )
+        
+        let output = viewModel.transform(input: input, disposeBag: disposeBag)
+        
+        output.startResult
+            .emit(onNext: { [weak self] isLogined in
+                if isLogined {
+                    self?.navigationController?.setViewControllers(
+                        [DIContainer.shared.makeLoginSuccessViewController()],
+                        animated: false
+                    )
+                } else {
+                    self?.navigationController?.pushViewController(
+                        DIContainer.shared.makeSignUpViewController(),
+                        animated: true
+                    )
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
