b181433e87311ada0f75ae4f926d5ab17b6142aa
ChoiAnYong <dksdyd78@naver.com>
2025-08-21 00:47:49 +0900

    feat: StartViewModel 구현

---
+++ iOSAssignment/iOSAssignment/Presentation/StartView/ViewModel/StartViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  StartViewModel.swift
+//  iOSAssignment
+//
+//  Created by 최안용 on 8/20/25.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+final class StartViewModel: ViewModelType {
+    private let userUseCase: UserUseCase
+    
+    init(userUseCase: UserUseCase) {
+        self.userUseCase = userUseCase
+    }
+    
+    struct Input {
+        let startTrigger: Observable<Void>
+    }
+    
+    struct Output {
+        let startResult: Signal<Bool>
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let result = input.startTrigger
+            .map { [weak self] in
+                guard let self = self else { return false }
+                return self.userUseCase.isLogined()
+            }
+            .asSignal(onErrorJustReturn: false)
+        
+        return Output(startResult: result)
+    }
+}
+
