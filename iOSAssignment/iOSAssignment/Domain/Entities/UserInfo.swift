2587d4ba1c5d94c5d08cbe8a59f52508174d512d
ChoiAnYong <dksdyd78@naver.com>
2025-08-21 15:17:21 +0900

    feat: UserInfo 구현

---
+++ iOSAssignment/iOSAssignment/Domain/Entities/UserInfo.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfo.swift
+//  iOSAssignment
+//
+//  Created by 최안용 on 8/20/25.
+//
+
+import Foundation
+
+struct UserInfo {
+    let id: String
+    let password: String
+    let nickname: String
+}
